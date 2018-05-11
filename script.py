7bea2b7fbba2be6817834bb2363671901731046c
SumitMadhale <smadhale.temp@gmail.com>
2018-05-11 10:18:23 +0530

    Added formatting with upper()

--- script.py
+++ script.py
@@ -1,2 +1,10 @@
+# input() formatting with lower()
+
 value=input("Enter the value :").lower()
 print("The value entered is:",value)
+
+# input() formatting with upper()
+
+input=input("Enter the input :").upper()
+print("The input is:",input)
+
